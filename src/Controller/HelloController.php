b6d163b37a9cd0dd3dc93982dafabd734f550f1b
Alexandr Gorlov <a.gorlov@gmail.com>
2025-09-18 19:07:33 +0300

    Load env bootstrap and add controller docs

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -5,8 +5,14 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+/**
+ * Контроллер, возвращающий приветственное сообщение на корневом маршруте.
+ */
 final class HelloController
 {
+    /**
+     * Отвечает текстом «Hello codex!» на запросы к главной странице.
+     */
     #[Route('/', name: 'app_home')]
     public function __invoke(): Response
     {
