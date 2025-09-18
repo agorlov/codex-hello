950db02edb50fb8cd9b2e90b0a017872df57a0bf
Alexandr Gorlov <a.gorlov@gmail.com>
2025-09-18 21:18:22 +0300

    Switch homepage rendering to Twig

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,20 +2,44 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 /**
  * Страница приветственного сообщения Codex.
  */
-final class HelloController
+final class HelloController extends AbstractController
 {
     /**
-     * Отвечает текстом «Hello codex!» на запросы к главной странице.
+     * Формирует и возвращает оформленную приветственную страницу Codex.
      */
     #[Route('/', name: 'app_home')]
     public function __invoke(): Response
     {
-        return new Response('Hello codex!');
+        return $this->render('homepage.html.twig', [
+            'greeting' => 'Hello codex!',
+            'pageTitle' => 'Codex приветствует вас',
+            'introText' => 'Создайте свой первый проект, экспериментируйте с компонентами Symfony и украшайте интерфейсы при помощи Tailwind.',
+            'documentationUrl' => 'https://symfony.com/doc/current/index.html',
+            'highlights' => [
+                [
+                    'title' => 'Symfony',
+                    'description' => 'Современный PHP-фреймворк, который помогает строить мощные и устойчивые приложения.',
+                ],
+                [
+                    'title' => 'Tailwind CSS',
+                    'description' => 'Утилитарный CSS-фреймворк, позволяющий быстро создавать аккуратные интерфейсы без лишнего кода.',
+                ],
+                [
+                    'title' => 'Быстрый старт',
+                    'description' => 'Используйте примеры, чтобы ускорить знакомство с фреймворком и его экосистемой.',
+                ],
+                [
+                    'title' => 'Комьюнити',
+                    'description' => 'Делитесь находками и задавайте вопросы сообществу Codex и Symfony.',
+                ],
+            ],
+        ]);
     }
 }
