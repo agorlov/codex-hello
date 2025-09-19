92d38b68801aa2c517aa67c896d7049fcc83e50b
Alexandr Gorlov <a.gorlov@gmail.com>
2025-09-19 09:08:05 +0300

    Rename homepage action to index

--- tests/Functional/HelloControllerTest.php
+++ tests/Functional/HelloControllerTest.php
@@ -2,22 +2,75 @@
 
 namespace App\Tests\Functional;
 
+use App\Greeting\RandomCodexGreeting;
+use ReflectionClass;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 /**
- * Проверяет, что контроллер приветствия отвечает ожидаемым текстом.
+ * Проверяет, что контроллер приветствия возвращает корректные фразы Codex.
  */
 class HelloControllerTest extends WebTestCase
 {
     /**
-     * Отправляет GET-запрос на корневой маршрут и проверяет ответ.
+     * Убеждается, что по умолчанию используется русское приветствие Codex.
      */
-    public function testHomepageDisplaysGreeting(): void
+    public function testHomepageDisplaysRussianGreetingByDefault(): void
     {
         $client = static::createClient();
-        $client->request('GET', '/');
+        $crawler = $client->request('GET', '/');
 
         $this->assertResponseIsSuccessful();
-        $this->assertStringContainsString('Hello codex!', $client->getResponse()->getContent());
+
+        $greeting = $crawler->filter('h1')->text();
+        $greetingsByLanguage = $this->getGreetingsByLanguage();
+
+        $this->assertContains($greeting, $greetingsByLanguage['ru']);
+    }
+
+    /**
+     * Проверяет, что параметр language=en включает английские приветствия Codex.
+     */
+    public function testHomepageDisplaysEnglishGreetingWhenRequested(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/?language=en');
+
+        $this->assertResponseIsSuccessful();
+
+        $greeting = $crawler->filter('h1')->text();
+        $greetingsByLanguage = $this->getGreetingsByLanguage();
+
+        $this->assertContains($greeting, $greetingsByLanguage['en']);
+    }
+
+    /**
+     * Проверяет, что неподдерживаемый язык откатывается к русскому приветствию.
+     */
+    public function testHomepageFallsBackToDefaultLanguageWhenUnsupported(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/?language=fr');
+
+        $this->assertResponseIsSuccessful();
+
+        $greeting = $crawler->filter('h1')->text();
+        $greetingsByLanguage = $this->getGreetingsByLanguage();
+
+        $this->assertContains($greeting, $greetingsByLanguage['ru']);
+    }
+
+    /**
+     * Возвращает карту доступных приветствий Codex, сгруппированных по языку.
+     *
+     * @return array<string, string[]>
+     */
+    private function getGreetingsByLanguage(): array
+    {
+        $reflection = new ReflectionClass(RandomCodexGreeting::class);
+
+        /** @var array<string, string[]> $greetings */
+        $greetings = (array) $reflection->getConstant('GREETINGS');
+
+        return $greetings;
     }
 }
